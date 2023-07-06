99c5548fd27ac7844616bdebc5452d3483be75e8
Michael <msousacode@gmail.com>
2023-07-06 15:14:55 -0300

    feat: aqui foi realizado o for de elementos na DOM e apresentação de um descritivo de teste

--- projeto2/app-task/src/App.vue
+++ projeto2/app-task/src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">    
+    <!--Tarefas é o componente central que gera e controla todas as tarefas-->    
+    <tarefas/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tarefas from './components/Tarefas.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'tarefas': Tarefas
   }
 }
 </script>
@@ -23,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px;  
 }
 </style>
---
+++ projeto2/app-task/src/components/Tarefas.vue
@@ -0,0 +1,50 @@
+<template>
+  <!--Componente Pai de todas as tarefas-->
+  <div id="tarefas">
+    {{ teste }}
+    <!--Recebe o descritivo da tarefa-->
+    <input type="text" />
+
+    <!--Cria a tarfefa adicionando a mesma dentro de um array itens onde ficam armazenadas todas as tarefas-->
+    <button type="button" @click="adicionarTarefa('Teste')">Nova tarefa</button>
+
+    <br />
+    <br />
+
+    <ul>
+      <!-- Aqui estou realizando a interação do array de objetos
+        Sendo que esses itens representarão as tarefas.-->
+      <li v-for="(item, index) in itens()" :key="item.message">
+        <div id="tarefa">{{ index + 1 }} - {{ item.message }}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: function () {
+    return {
+      teste: "Palmeiras",
+    };
+  },
+  methods: {
+    //A função itens retorna o array de objetos que representa as minhas tarefas.
+    itens() {
+      return [{ message: "Algo" }, { message: "Outro" }];
+    },
+    adicionarTarefa(value) {
+      console.info(value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+#tarefa {
+  padding: 10px;
+  margin: 10px;
+  border: solid 1px green;
+}
+</style>
